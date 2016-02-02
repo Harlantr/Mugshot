533c4abda4748a1e024016c9d4548dee3cf33a8a
Tom Harlan <harlantr@mail.uc.edu>
2016-02-02 18:36:57 -0500

    Layout/Image changes. Pop out window for mug info

--- app/scripts/controllers/mugs.js
+++ app/scripts/controllers/mugs.js
@@ -12,5 +12,12 @@ angular.module('mugshotApp')
     $scope.MugsList = [];
     $http.get('mugs.json').success(function(data){
       $scope.MugsList = data;
-    });
+    })
+
+    $scope.openMugInfo = function (url, name, description){
+    	document.getElementById('divMugInfo').setAttribute('class', 'mug-info');
+    	document.getElementById('imgMug').src = url;
+    	document.getElementById('divMugName').innerHTML = name;
+    	document.getElementById('lblMugDesc').innerHTML = description;
+    };
   });
